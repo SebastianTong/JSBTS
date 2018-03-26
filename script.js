5cf0bb324f1e01c39b982b47f09a27321c539049
SebastianTong <tongsebastian27@gmail.com>
2018-03-26 16:21:18 -0300

    hoisting

--- script.js
+++ script.js
@@ -1,77 +1,29 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// Functions
 
+calculateAge(1994);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////
-// Lecture: Scoping
-
-
-// First scoping example
-
-/*
-var a = 'Hello!';
-first();
-
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
-
-
-
-// Example to show the differece between execution stack and scope chain
-
-/*
-var a = 'Hello!';
-first();
-
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        third()
-    }
+function calculateAge(year) {
+    console.log(2018 - year);
 }
 
-function third() {
-    var d = 'John';
-    console.log(a + b + c + d);
+// retirement(1994);  this wont work because hoisting is only for function declarations and not expressions.
+
+var retirement = function(year){
+    console.log(65 - (2018 - year));
 }
-*/
 
+//Variables
 
+console.log(age); // Js knows there is a variable declaration but doesnt know the value yet because it reads line by line.
+var age = 24; //if we delete this line, then there is no declaration and js will return an error.
 
-///////////////////////////////////////
-// Lecture: The this keyword
+function foo() {
+    var age = 33;
+    console.log(age);
+}
 
-
-
-
-
-
-
-//test
-
+foo(); // this returned the variable age 33 because its inside the variable object of the function context box.
+console.log(age); // this returned age 24 because its declared in the variable object of the global context box.
